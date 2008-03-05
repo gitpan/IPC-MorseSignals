fe127685ba954a43d085c0beab6bab7ac81f5d35
Vincent Pit <perl@profvince.com>
2008-03-05 21:08:38 +0000

    Import of VPIT/IPC-MorseSignals-0.11 from CPAN.
    gitpan-cpan-distribution: IPC-MorseSignals
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         VPIT/IPC-MorseSignals-0.11.tar.gz
    gitpan-cpan-author:       VPIT
    gitpan-cpan-maturity:     released

--- lib/IPC/MorseSignals/Emitter.pm
+++ lib/IPC/MorseSignals/Emitter.pm
@@ -16,11 +16,11 @@ IPC::MorseSignals::Emitter - Base class for IPC::MorseSignals emitters.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
--- lib/IPC/MorseSignals/Receiver.pm
+++ lib/IPC/MorseSignals/Receiver.pm
@@ -14,11 +14,11 @@ IPC::MorseSignals::Receiver - Base class for IPC::MorseSignals receivers.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
